2b0473c297c984f5978e7789cc9c507dddbd837a
Bimsara Wickramarathne <bimbi2254@gmail.com>
2023-07-25 11:31:37 +0530

    Changes done to the vue app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# customer-view
+# DineEase
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ src/components/SignUp.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="sign-up-box">
+        <div class="sign-up-left">
+            <div class="logo">
+                <img src="../assets/dine ease logo color.png" alt="">
+            </div>
+            <div class="sign-up-form">
+                <h1>Register</h1>
+                <label for="reg-form">Name</label>
+                <input type="text">
+                <label for="reg-form">Email</label>
+                <input type="text">
+                <label for="reg-form">Contact Number</label>
+                <input type="text">
+                <label for="reg-form">Password</label>
+                <input type="text">
+                <label for="reg-form">Confirm Password</label>
+                <input type="text">
+                <input type="button" value="Register">
+                <p>Already have an account? <a href="#">Login</a></p>
+                <div class="or">
+                    <hr>
+                    <p>OR</p>
+                    <hr>
+                </div>
+                <button>
+                    <img src="../assets/google.png" alt="">
+                    <p>Continue with Google</p>
+                </button>
+            </div>
+        </div>
+        <div class="sign-up-right">
+            
+        </div>
+    </div>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'SignUp',
+    data() {
+        return {
+            title: 'Sign Up Page'
+        };
+    }
+});
+</script>
