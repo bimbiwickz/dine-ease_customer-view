a25dab1bf03127c73e9356a74c5bd3f3fd1b109b
Bimsara Wickramarathne <bimbi2254@gmail.com>
2023-07-27 16:26:14 +0530

    SignUp updated

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,42 +1,45 @@
+<style>
+    @import '@/assets/main.css';
+</style>
 <template>
-    <div class="sign-up-box">
-        <div class="sign-up-left">
-            <div class="logo">
-                <img src="../assets/dine ease logo color.png" alt="">
+    <div class="container items-center mx-auto bg-green-500 pl-72 pt-32 ">
+      <div class="container w-3/4 max-h-max bg-white content-center py-6 px-6 rounded-3xl flex-columns ">
+        <div class="sign-up-left w-1/2 items-center ">
+          <div class="logo">
+            <img class="w-36 h-36" src="../assets/dine ease logo color.png" alt="">
+          </div>
+          <div class="sign-up-form ">
+            <h1 class="font-sans text-3xl text-center font-semibold pb-10 ">Register</h1>
+            <label for="reg-form" class="text-gray-700">Name</label><br>
+            <input type="text" class="w-2/3 border-lgray border-2 rounded-md p-2 hover:border-green"><br>
+            <label for="reg-form" class="text-gray-700">Email</label>
+            <input type="text" class="w-full border border-gray-300 rounded-md p-2">
+            <label for="reg-form" class="text-gray-700">Contact Number</label>
+            <input type="text" class="w-full border border-gray-300 rounded-md p-2">
+            <label for="reg-form" class="text-gray-700">Password</label>
+            <input type="text" class="w-full border border-gray-300 rounded-md p-2">
+            <label for="reg-form" class="text-gray-700">Confirm Password</label>
+            <input type="text" class="w-full border border-gray-300 rounded-md p-2">
+            <input type="button" value="Register" class="w-full bg-green text-white font-bold py-2 px-4 rounded-md">
+            <p class="text-gray-700">Already have an account? <a href="#" class="text-blue-500">Login</a></p>
+            <div class="or flex items-center my-4">
+              <hr class="flex-grow border-gray-300">
+              <p class="mx-4 text-gray-700">OR</p>
+              <hr class="flex-grow border-gray-300">
             </div>
-            <div class="sign-up-form">
-                <h1>Register</h1>
-                <label for="reg-form">Name</label>
-                <input type="text">
-                <label for="reg-form">Email</label>
-                <input type="text">
-                <label for="reg-form">Contact Number</label>
-                <input type="text">
-                <label for="reg-form">Password</label>
-                <input type="text">
-                <label for="reg-form">Confirm Password</label>
-                <input type="text">
-                <input type="button" value="Register">
-                <p>Already have an account? <a href="#">Login</a></p>
-                <div class="or">
-                    <hr>
-                    <p>OR</p>
-                    <hr>
-                </div>
-                <button>
-                    <img src="../assets/google.png" alt="">
-                    <p>Continue with Google</p>
-                </button>
-            </div>
+            <button class="flex items-center">
+              <img class="w-1/2" src="../assets/google.png" alt="">
+              <p class="ml-2">Continue with Google</p>
+            </button>
+          </div>
         </div>
-        <div class="sign-up-right">
-            
+        <div class="sign-up-right w-1/2">
+          <!-- Add content for the right side if needed -->
+          <img src="../assets/images/1.png" alt="">
         </div>
+      </div>
     </div>
-    <div>
-        <h1>{{ title }}</h1>
-    </div>
-</template>
+  </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'white': '#FFFFFF',
+      'green': '#007400',
+      'yellow': '#FFC700',
+      'lgreen': '#ECF8F8',
+      'txt-gray': '#4F5052',
+      'lgray': '#898585',
+    },
+    fontFamily: {
+      'sans': ['Inter', 'sans-serif'],
+      'serif': ['Playfair Display', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
