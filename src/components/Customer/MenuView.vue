547d836830395d99445e5a457f2c00d07f2c81d1
indzTracks <2020cs126@stu.ucsc.cmb.ac.lk>
2023-10-28 15:37:14 +0530

    modified UIs

--- src/components/Customer/MenuView.vue
+++ src/components/Customer/MenuView.vue
@@ -1,18 +1,22 @@
 <script lang="ts">
 import FoodCard from '../utils/food-card.vue';
+import SearchBar from '../utils/baseSearchBar.vue';
+import NavBar from '../utils/navBar-user.vue';
 import Image1 from '../../assets/images/jason-leung-poI7DelFiVA-unsplash (1).jpg';
 import { computed } from 'vue';
 
 export default {
   name: 'MenuView',
   components: {
-    FoodCard
+    FoodCard,
+    SearchBar,
+    NavBar
   },
   data() {
     return {
       foods: [
         { id: 1, title: 'Pizza', name: 'Magarita', price: 'Rs 1000.00', image: Image1 },
-        { id: 2, title: 'Burger', name: 'Veggie', price: '\\\$8', image: Image1 },
+        { id: 2, title: 'Burger', name: 'Vegitarian', price: '\\\$8', image: Image1 },
         { id: 3, title: 'Pasta', name: 'Cheese', price: '\\\$12', image: Image1 },
         { id: 4, title: 'Salad', name: 'Chicken', price: '\\\$7', image: Image1 },
         { id: 5, title: 'Steak', name: 'Beef', price: '\\\$15', image: Image1 },
@@ -24,25 +28,39 @@ export default {
       ],
       filter: '',
       selectedFilter: '', // Initially no filter selected
+      searchQuery: '',
+      pageItems: ['All Categories','Main Dishes','Desserts','Beverages','Alchohol'],
+      mainDishes: ['Pizza','Burger','Pasta','Sushi'],
+      desserts: ['Curry', 'Steak', 'Salad', 'Sandwich']
     };
   },
   computed: {
     computedFilteredFoods() {
-      if (this.filter) {
-        return this.foods.filter(food => food.name.toLowerCase() === this.filter.toLowerCase());
+      if (this.selectedFilter === 'Main Dishes') {
+        return this.foods.filter(food => this.mainDishes.includes(food.title));
+      }else if (this.selectedFilter === 'Desserts') {
+        return this.foods.filter(food => this.desserts.includes(food.title));
       }
       return this.foods;
     }
   },
   methods: {
     applyFilter() {
-      if (this.selectedFilter === '') {
+      if (this.selectedFilter === 'Main Dishes') {
+        // If "Main Dishes" filter selected, show only specific foods
+        this.filter = 'Pizza, Burger, Pasta, Sushi';
+      }else if (this.selectedFilter === 'Desserts') {
+        // If "Main Dishes" filter selected, show only specific foods
+        this.filter = 'Curry, Steak, Salad, Sandwich';
+      } else {
         // If no filter selected, show all foods
         this.filter = '';
-      } else {
-        // Filter the foods based on the selected filter
-        this.filter = this.selectedFilter.toLowerCase();
       }
+    },
+    applySearch() {
+      // Triggered when the search input value changes
+      // Update the searchQuery property and recompute the filtered foods
+      this.searchQuery = this.searchQuery.trim();
     },
   },
   created() {
@@ -54,6 +72,11 @@ export default {
 
 <template>
   <div>
+    <NavBar></NavBar>
+    <SearchBar
+    :items="pageItems"
+    @select="selectedFilter = $event"
+    ></SearchBar>
     <div class="filter-container">
       <label for="filter">Filter:</label>
       <select id="filter" v-model="selectedFilter" @change="applyFilter">
---
+++ src/components/utils/baseSearchBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <form>
+    <div class="flex">
+      <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only">Your Email</label>
+      <button id="dropdown-button" @click="toggleDropdown" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100" type="button">
+        All categories
+        <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+        </svg>
+      </button>
+      <div id="dropdown" :class="{ 'hidden': !dropdownVisible }" class="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
+        <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdown-button">
+          <li v-for="item in items" :key="item">
+            <button @click="selectItem(item)" type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100">{{ item }}</button>
+          </li>
+        </ul>
+      </div>
+      <div class="relative w-full">
+        <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required>
+        <button type="submit" class="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+          <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+          </svg>
+          <span class="sr-only">Search</span>
+        </button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    items: {
+      type: Array as () => string[],
+      required: true
+    }
+  },
+  data() {
+    return {
+      dropdownVisible: false
+    }
+  },
+  methods: {
+    toggleDropdown() {
+      this.dropdownVisible = !this.dropdownVisible;
+    },
+    selectItem(item) {
+      this.$emit('select', item);
+    }
+  }
+}
+</script>
