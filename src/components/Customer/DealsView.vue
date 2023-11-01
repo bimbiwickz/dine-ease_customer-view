7516514aa219960a3c9b4f67ec204a2bc047e57e
Bimsara Wickramarathne <bimbi2254@gmail.com>
2023-11-01 20:44:13 +0530

    Navbar updated

---
+++ src/components/Customer/DealsView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <NavBar></NavBar>    
+      <pg-title class="py-24 flex justify-center text-xl text-green">
+        See the offers available for you!
+      </pg-title>
+      <div class="offers flex flex-col items-center space-y-4">
+        <!-- First offer image -->
+        <img src="../../assets/ad-1.png" alt="Offer 1" class="w-1/2 h-72 object-cover rounded-lg">
+        <!-- Second offer image -->
+        <img src="../../assets/ad-2.png" alt="Offer 2" class="w-1/2 h-72 object-cover rounded-lg">
+      </div>
+      <container class="container flex flex-row flex-wrap p-2 xs:p-0 justify-center">
+        <div class="grid lg:grid-cols-4 md:grid-cols-3 xs:grid-cols-2">
+          <!-- Other content goes here -->
+        </div>
+      </container>
+    </div>
+</template>
+  
+
+<script lang="ts">
+    import NavBar from '../utils/navBar-user.vue';
+    export default {
+        name: 'MenuView',
+        components: {
+            NavBar
+        }
+    }
+</script>
+  
--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,7 @@ import UserProfile from './components/Customer/UserProfileDashboard.vue'
 import Reservations from './components/Customer/Reservations.vue'
 import addFood from './components/Manager/addFood.vue'
 import addMeal from './components/Manager/addMeal.vue'
+import Deals from './components/Customer/DealsView.vue'
 
 // import Vue3ConfirmDialog from '@zapadale/vue3-confirm-dialog'
 
@@ -35,11 +36,6 @@ export interface globalcomponents {
   BaseActionBtn: typeof import('./components/utils/baseActionBtn.vue')
 }
 const routes = [
-    // {
-    //     path: '/baseInput',
-    //     component: baseInput,
-    //     //meta: { requiresAuth: true },
-    // },
     {
         path: '/',
         component: Home,
@@ -105,6 +101,12 @@ const routes = [
         props: true,
     },
     {
+        path: '/customer/DealsView',
+        name: 'deals',
+        component: Deals,
+        props:true,
+    },
+    {
         path: '/user-profile',
         component: UserProfile, 
     },
@@ -122,10 +124,7 @@ const routes = [
       path: '/add-meal',
       component: addMeal, 
   },
-  //     path: '/baseInput',
-  //     component: baseInput,
-  //     //meta: { requiresAuth: true },
-  // },
+
   {
     path: '/',
     component: Home
